aac65ddd0a47cb81ba173c5f46134d410ca0b8d0
Samuel-101 <117648593+Samuel-101@users.noreply.github.com>
2024-09-05 15:55:18 +0530

    Update half_adder.vhd

--- projects/verification/half_adder.vhd
+++ projects/verification/half_adder.vhd
@@ -1,21 +1,20 @@
 library ieee;
-    use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.all;                                      -- Importing the IEEE library, specifically for standard logic (std_logic and std_ulogic types).
 
-    entity half_adder is
-    port(
+entity half_adder is                                              -- Declares the entity for the half adder circuit.
 
-        a: in std_ulogic;
-        b: in std_ulogic;
-        o: out std_ulogic;
-        c: out std_ulogic
-        
-        );
+port(
+    a: in std_ulogic;                                             -- Input signal 'a' of type std_ulogic (unresolved logic).
+    b: in std_ulogic;                                             -- Input signal 'b' of type std_ulogic (unresolved logic).
+    o: out std_ulogic;                                            -- Output signal 'o' for the sum of 'a' and 'b', also of type std_ulogic.
+    c: out std_ulogic                                             -- Output signal 'c' for the carry of 'a' and 'b', of type std_ulogic.
+    );
 
-    end half_adder;
+end half_adder;                                                  -- End of the entity declaration.
 
-    architecture behave of half_adder is 
-    begin 
-        o<= a xor b;
-        c<=a and b;
+architecture behave of half_adder is                             -- The architecture "behave" defines the behavior of the half adder.
+begin 
+    o <= a xor b;                                                -- The sum output 'o' is assigned the result of XOR operation between 'a' and 'b'.
+    c <= a and b;                                                -- The carry output 'c' is assigned the result of AND operation between 'a' and 'b'.
 
-    end behave;
+end behave;                                                      -- End of the architecture.
