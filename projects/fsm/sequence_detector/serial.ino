b7dfc1c85dc99195269176efef11e9da21e763ff
Samuel-101 <117648593+Samuel-101@users.noreply.github.com>
2024-09-06 11:10:21 +0530

    Update serial.ino

--- projects/fsm/sequence_detector/serial.ino
+++ projects/fsm/sequence_detector/serial.ino
@@ -1,21 +1,21 @@
-    const int clockPin = 2; // Pin for clock signal
-    const int dataPin = 5;  // Pin for data signal
-    void setup() {
+    const int clockPin = 2;                // Pin for clock signal
+    const int dataPin = 5;                 // Pin for data signal
+    void setup() {                         // In this code the clock is created instead of specifying a baudrate, this clock is sent to fpga for synchronisation.
     pinMode(clockPin, OUTPUT);
     pinMode(dataPin, OUTPUT);
     Serial.begin(9600);
     }
 
     void loop() {
-    int seq[100]={0,1,0,1,1,0};//input sequence
+    int seq[100]={0,1,0,1,1,0};            //input sequence
     for(int i=0;i<100;i++)
     {
     digitalWrite(clockPin, HIGH);
-    delay(500); // clock frequency set to 1Hz
+    delay(500);                             // clock frequency set to 1Hz
     digitalWrite(clockPin, LOW);
     delay(500);
     digitalWrite(dataPin,seq[i]);
-    Serial.println(seq[i]);//display output to serial monitor
+    Serial.println(seq[i]);                //display output to serial monitor
     
     }
     }
